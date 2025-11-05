58d439205d531b7eabf919c3eb96bcd599c1a3e5
Mirmex11 <mirmex981@yandex.ru>
2025-11-05 20:40:56 +0300

    Добавлены файлы: .sln .csproj, файлу с кодом было изменено расширение на .cs и были убраны пустые строки

--- Code/Code/Code.cs
+++ Code/Code/Code.cs
@@ -1,8 +1,12 @@
-using System;
+﻿using System;
+
  public class Program
 {
     public static void BubbleSort(int[] array)
-    { 
+    {
+        if (array == null) {
+            Console.WriteLine("Массив пуст")
+        }
         int n = array.Length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) 
@@ -14,8 +18,9 @@ using System;
                     array[j + 1] = temp;
                 }
             }
-        }
+        } 
     }
+
     public static void Main(String[] array)
     {
         int[] myArray = { 54, 3, 22, 34, 65, 77, 5 };
